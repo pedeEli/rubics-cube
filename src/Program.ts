bee842a2602d19be893ebd773be80d0db38d5cb1
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-03-15 00:57:59 +0100

    added a uniform map to program

--- src/Program.ts
+++ src/Program.ts
@@ -1,28 +1,18 @@
-const vertexRegex = /(?<=#Vertex\r?\n)([\w\W]*?)(?=#Fragment|$)/
-const fragmentRegex = /(?<=#Fragment\r?\n)([\w\W]*?)(?=#Vertex|$)/
-
 class Program {
     private program: WebGLProgram | null = null
+    private uniformMap: Map<string, number>
 
-    public constructor(private path: string, private gl: WebGL2RenderingContext) {}
-
-    public async compileAndLinkPorgram() {
-        const data = await fetch(this.path)
-        const shaderStr = await data.text()
-        const vertexShaderMatch = shaderStr.match(vertexRegex)
-        const fragmentShaderMatch = shaderStr.match(fragmentRegex)
-        if (!vertexShaderMatch?.length)
-            throw new Error(`No vertex shader defined in '${this.path}'. Make sure to write '#Vertex' before your vertex shader`)
-        if (!fragmentShaderMatch?.length)
-            throw new Error(`No fragment shader defined in '${this.path}'. Make sure to write '#Fragment' before your fragment shader`)
-
-        const vertexShaderSource = vertexShaderMatch[0]
-        const fragmentShaderSource = fragmentShaderMatch[0]
-
+    public constructor(
+        private path: string,
+        vertexShaderSource: string,
+        fragmentShaderSource: string,
+        private gl: WebGL2RenderingContext
+    ) {
         const vertexShader = this.createShader(vertexShaderSource, this.gl.VERTEX_SHADER, 'vertex')
         const fragmentShader = this.createShader(fragmentShaderSource, this.gl.FRAGMENT_SHADER, 'fragment')
 
         this.program = this.gl.createProgram()
+        
         if (!this.program)
             throw new Error('Fatal: webgl could not create program object!')
 
@@ -30,13 +20,20 @@ class Program {
         this.gl.attachShader(this.program, fragmentShader)
         this.gl.linkProgram(this.program)
         const success = this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)
-        if (success)
-            return
-        
-        const info = this.gl.getProgramInfoLog(this.program)
-        this.gl.deleteProgram(this.program)
-        this.program = null
-        throw new Error(`Link Program: ${info}`)
+        if (!success) {            
+            const info = this.gl.getProgramInfoLog(this.program)
+            this.gl.deleteProgram(this.program)
+            this.program = null
+            throw new Error(`Link Program: ${info}`)
+        }
+
+        const numUniforms = this.gl.getProgramParameter(this.program, this.gl.ACTIVE_UNIFORMS) as number
+        const uniformIndices = [...Array(numUniforms).keys()]
+        const uniformNames = uniformIndices.map(index => {
+            const info = this.gl.getActiveUniform(this.program!, index)
+            return [info!.name, index] as const
+        })
+        this.uniformMap = new Map(uniformNames)
     }
 
     private createShader(source: string, type: number, typeStr: string) {
@@ -53,6 +50,12 @@ class Program {
         const info = this.gl.getShaderInfoLog(shader)
         this.gl.deleteShader(shader)
         throw new Error(`Compile '${this.path}': ${typeStr}: ${info}`)
+    }
+
+    public use() {
+        if (!this.program)
+            throw new Error('Fatal: program does not exists!')
+        this.gl.useProgram(this.program)
     }
 }
 

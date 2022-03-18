ff528db164e0f4f5aa80fe553c72c2be906edea9
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-03-18 02:45:15 +0100

    added smoothing to the inside edge of the rime

--- src/shader/cube.glsl.ts
+++ src/shader/cube.glsl.ts
@@ -24,10 +24,27 @@ out vec4 FragColor;
 
 uniform vec3 color;
 
+vec3 rime = vec3(0.07);
+float outer = 0.45;
+float inner = 0.44;
+
+float map(float value, float min1, float max1, float min2, float max2)
+{
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
 void main()
 {
-    if (abs(pos.x) > 0.45 || abs(pos.y) > 0.45) {
-        FragColor = vec4(vec3(0.07), 1.0);
+    float x = abs(pos.x);
+    float y = abs(pos.y);
+    if (x > outer || y > outer) {
+        FragColor = vec4(rime, 1.0);
+        return;
+    }
+    if (x > inner || y > inner) {
+        float t = smoothstep(0.0, 1.0, map(max(x, y), outer, inner, 0.0, 1.0));
+        vec3 c = color * t + rime * (1.0 - t);
+        FragColor = vec4(c, 1.0);
         return;
     }
     FragColor = vec4(color, 1.0);
